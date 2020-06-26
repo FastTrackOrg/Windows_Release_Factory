7f5d492ff0a0bae01e3ce0a81bc23574f98f18a4
bgallois <benjamin@gallois.cc>
2020-06-26 16:15:25 +0200

    Add windows installer creation script

---
+++ window_installer/deploy.bat
@@ -0,0 +1,3 @@
+C:\Qt\QtIFW-3.2.2\bin\repogen.exe -p C:\Users\benjamin\fishytrackingdeployement\FastTrack\packages FastTrackRep
+C:\Qt\QtIFW-3.2.2\bin\binarycreator.exe -t C:\Qt\QtIFW-3.2.2\bin\installerbase.exe -p FastTrack\packages -c FastTrack\config\config.xml FastTrackInstaller
+pause
