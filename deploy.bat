ade9c9f8860c8d961877b28349852af0c1e81d17
bgallois <benjamin@gallois.cc>
2021-10-26 12:45:36 +0200

    Update CI to last version of aqtinstall

--- deploy.bat
+++
@@ -1,3 +0,0 @@
-set PATH=.\Qt\Qt\Tools\QtInstallerFramework\4.1\bin
-repogen.exe -p .\FastTrack\packages FastTrackRep
-binarycreator.exe -p .\FastTrack\packages -c .\FastTrack\config\config.xml FastTrackInstaller
